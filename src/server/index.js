11a63a98b6c0323ab08dc24e698b01a394c0f2c2
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2019-05-31 23:51:47 +0200

    heroku - enable dynamic port when deployed by heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const os = require('os');
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
